f7d88f8b9ba677488e5e4819909fa9caa99d44b4
Prakash-lenka <jayaprakashnaidulenka@gmail.com>
2026-07-23 12:10:16 +0530

    first commit

---
+++ J1.java
@@ -0,0 +1,17 @@
+// ...existing code...
+import java.util.Arrays;
+
+public class J1 {
+   public static void main(String[] args) {
+       System.out.println("Hello, World!");
+       System.out.println("Hello, World!");
+       System.out.println("Hello, World!");
+       int one = 1;
+       int two = 2;
+       int a = one + two * two + one;
+       a += one;
+       String str = "a b c";
+       String[] s = str.split(" ");
+       System.out.println(Arrays.toString(s));
+   }
+}
